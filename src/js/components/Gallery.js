e3a813a3bdb7e1c2d5375d914e3d4a91c467164c
Oleg Mikhaylov <dkfolkin@gmail.com>
2021-08-20 18:50:27 +0300

    feat: started with slider

---
+++ src/js/components/Gallery.js
@@ -0,0 +1,10 @@
+import render from "../helper.render";
+import markup from "../markup";
+
+class Gallery {
+    sayHi() {
+        console.log('Hi');
+    }
+}
+
+export default Gallery;
--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import 'normalize.css';
 import '../sass/style.scss';
 
 import Nav from "./components/nav";
+import Gallery from "./components/Gallery";
 
 document.addEventListener("DOMContentLoaded", () => {
     const nav = new Nav();
